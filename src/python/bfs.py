0cec2187ea70de7f2d46cdbaf6c2ba148ea012a6
Manoel Vilela <manoel_vilela@engineer.com>
2019-05-29 11:51:51 -0300

    Add bfs template

---
+++ src/python/bfs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import maps
+
+
+def bfs(graph, a, b):
+    """Algoritmo de Busca em Largura.
+
+    Complexidade de espaço: O(b^d)
+    Complexidade de tempo: O(b^d)
+
+    Prioridade de busca:
+    1. Cria raiz
+    2. Cria filhos esquerda e direita
+    3. Seleciona nó esquerdo como raiz
+    4.
+
+    Algoritmo é ótimo. (?)
+    Algoritmo é completo.
+    """
+    pass
