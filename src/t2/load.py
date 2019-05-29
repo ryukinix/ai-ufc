1e04d9b669a5e693a94a43eb03199fe28525e342
Manoel Vilela <manoel_vilela@engineer.com>
2019-05-29 12:18:51 -0300

    Add load module and data/ for t2

---
+++ src/t2/load.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+import numpy as np
+
+
+def dataset(fname):
+    """Load a .dat file as numpy array-matrix"""
+    with open(fname) as f:
+        data = []
+        for line in f.readlines():
+            numbers = line.split()
+            data.append([float(num) for num in numbers])
+        return np.array(data)
+
+
+
+def aerogerador():
+    """
+    Columns description
+    -------------------
+
+    0: velocidade do vento (m/s)
+    1: potência gerada (kW)
+
+    x -> 0
+    y -> 1
+    """
+    d = dataset('data/aerogerador.dat')
+    x, y = d[:, 0], d[:, 1]
+    return x, y
+
+
+def iris():
+    """
+    Columns description
+    -------------------
+    0: caracteristica 1
+    1: caracteristica 2
+    2: caracteristica 3
+    3: caracteristica 4
+    4: classe 1
+    5: classe 2
+    6: classe 3
+
+    X -> [0, 1, 2, 3]
+    y -> [4, 5, 6]
+    """
+    d = dataset('data/iris_log.dat')
+    X, y = d[:, 0:4], d[:, -3:]
+    return X, y
