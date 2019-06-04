25bba20d648890917bd123ceb766cf8ea39e4b4d
Manoel Vilela <manoel_vilela@engineer.com>
2019-06-04 17:09:24 -0300

    q1.py: polinomial regression support and make text report pretty

--- src/t2/q1.py
+++ src/t2/q1.py
@@ -26,32 +26,61 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def train(X, y, q=2):
+    """Treina uma rede neural ELM para regressão com q neurônios ocultos.
+
+    Return
+    ------
+    r2: coeficiente de ajuste da curva
+    y_pred: valores estimados de regressão para X
+    """
     W, M = elm.train(X, y, q=q)
     y_pred = elm.predict(X, W, M)
     r2 = testing.r2(y.flatten(), y_pred.flatten())
 
     return r2, y_pred
 
+def polinomial(X, k=1):
+    """Gera features para regressão polinomial de grau k."""
+    n = len(X)
+    if k == 1:
+        return X
+    # acumulador de potências de x
+    pots = []
+    for i in range(1, k+1):
+        x = X ** i
+        pots.append(x.reshape(n, 1))
+
+    return np.concatenate(pots, axis=1)
+
 
 def main():
     print(__doc__)
     X, y = load.aerogerador()
     X, y = colv(X), colv(y)
-    # Plotting
-    # fig 1: models
+    x = X
+    k = 5
+    q_range = (2, 12, 4)
+    X = polinomial(X, k)
+    print(":: Grau polinômio k={}".format(k))
+
     plt.figure("Inteligência Computacional 2019.1 UFC")
-    for q in range(1, 10, 2):
+    print("+---+-------------------+")
+    print("| q | r2                |")
+    print("|---+-------------------|")
+    for q in range(*q_range):
         r2, y_pred = train(X, y, q=q)
         label = 'q={} r²={}'.format(q, round(r2, ndigits=3))
-        plt.plot(X, y_pred.flatten(), label=label)
+        print("| {:2d}| {}|".format(q, r2))
+        plt.plot(x, y_pred.flatten(), label=label)
+    print("+---+-------------------+")
 
-    plt.scatter(X, y, color='black', s=3)
+    plt.scatter(x, y, color='black', s=3)
     ax = plt.gca()
-    ax.set_title("Regressão via Extreme Learning Machine")
+    ax.set_title("Regressão via Extreme Learning Machine para k={}".format(k))
     ax.set_xlabel("x: velocidade do vento (m/s)")
     ax.set_ylabel("y: potência gerada (KWatt)")
     ax.legend()
-    plt.savefig("pics/q1-elm.png", figsize=(10, 8))
+    plt.savefig("pics/q1-elm-polinomial.png", figsize=(10, 8))
     plt.show()
 
 
