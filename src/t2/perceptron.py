cc47bf66f443260219b1c140b897e88f683d3ac9
Manoel Vilela <manoel_vilela@engineer.com>
2019-06-04 16:14:37 -0300

    Add perceptron file boilerplate

---
+++ src/t2/perceptron.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#    Copyright © 2019 Manoel Vilela
+#
+#    @project: Inteligência Computacional UFC 2019.1 - Redes Neurais
+#     @author: Manoel Vilela
+#      @email: manoel_vilela@engineer.com
+#
