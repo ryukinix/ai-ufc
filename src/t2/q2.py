8404551c9ec55ecd4f8f2632e6793e950d18a2f9
Manoel Vilela <manoel_vilela@engineer.com>
2019-06-04 16:13:36 -0300

    Add q2.py boilerplate

---
+++ src/t2/q2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#    Copyright © 2019 Manoel Vilela
+#
+#    @project: <project>
+#     @author: Manoel Vilela
+#      @email: manoel_vilela@engineer.com
+#
+
+
+"""
+Classifique o conjunto de dados disponível no arquivo iris_log.dat
+usando: Perceptron, MLP, e ELM. Utilize as estratégias de validação:
+hold-out (70% treino, 30% teste), 10-fold e leave-one-out.
+"""
