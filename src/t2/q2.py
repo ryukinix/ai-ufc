d85054edaaa9dd4fa719a1156e1be00a64594de7
Manoel Vilela <manoel_vilela@engineer.com>
2019-06-04 21:28:51 -0300

    Add MLP experiment using scikit-learn implementation

--- src/t2/q2.py
+++ src/t2/q2.py
@@ -3,7 +3,7 @@
 #
 #    Copyright © 2019 Manoel Vilela
 #
-#    @project: <project>
+#    @project: Inteligência Computacional UFC 2019.1 - Redes Neurais
 #     @author: Manoel Vilela
 #      @email: manoel_vilela@engineer.com
 #
@@ -21,11 +21,67 @@ import testing
 import elm
 import perceptron
 import numpy as np
+from sklearn.neural_network import MLPClassifier
 
-def train_test_perceptron(X, y):
-    pass
+def mlp(q):
+    """Cria uma instância do classificador Multi-Layer Perceptron com
+    q neurônios na camada oculta.
 
-def evaluate_elm(X_train, X_test, y_train, y_test, q=10):
+    Como optimizador utilize SGD: Sthocastic Gradient Descent
+
+    Implementação utilizada: sklearn
+
+    Documentação: https://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html
+    """
+    return MLPClassifier(solver='sgd',
+                         max_iter=3000,
+                         hidden_layer_sizes=(q,),
+                         random_state=processing.SEED)
+
+def evaluate_mlp(X_train, X_test, y_train, y_test, q):
+    # HOLD-OUT
+    clf = mlp(q)
+    clf.fit(X_train, processing.encode_label(y_train))
+    y_pred = clf.predict(X_test)
+    y_test = processing.encode_label(y_test)
+    acc = testing.accuracy(y_test, y_pred)
+    return acc
+
+
+
+
+def train_test_mlp(X, y, q):
+    accs = []
+
+    # HOLD-OUT
+    X_train, X_test, y_train, y_test = testing.hold_out(X, y, test_size=0.3)
+    acc = evaluate_mlp(X_train, X_test, y_train, y_test, q)
+    accs.append(acc)
+
+
+    # ==> K-FOLD, k=10
+    accs_kfold = []
+    folds = testing.kfold(X, y, k=10)
+    for X_train, X_test, y_train, y_test in folds:
+        acc = evaluate_mlp(X_train, X_test, y_train, y_test, q)
+        accs_kfold.append(acc)
+    acc_kfold = np.mean(np.array(accs_kfold))
+    accs.append(acc_kfold)
+
+
+    # ==> LEAVE ONE OUT
+    accs_leave = []
+    folds = testing.leave_one_out(X, y)
+    for X_train, X_test, y_train, y_test in folds:
+        acc = evaluate_mlp(X_train, X_test, y_train, y_test, q)
+        accs_leave.append(acc)
+    acc_leave = np.mean(np.array(accs_leave))
+    accs.append(acc_leave)
+
+
+    return accs
+
+def evaluate_elm(X_train, X_test, y_train, y_test, q):
     # train
     W, M = elm.train(X_train, y_train, q=q, activation=processing.sigmoid)
 
@@ -34,8 +90,11 @@ def evaluate_elm(X_train, X_test, y_train, y_test, q=10):
     y_test = processing.encode_label(y_test)
     y_pred = processing.encode_label(y_pred_class)
     acc = round(testing.accuracy(y_test, y_pred), ndigits=2)
+
     return acc
 
+def train_test_perceptron(X, y):
+    pass
 
 def train_test_elm(X, y, q=40):
     """Treina, testa e calcúla acurácia do ELM para q.
@@ -73,11 +132,10 @@ def train_test_elm(X, y, q=40):
 
     return accs
 
-def train_test_mlp(X, y):
-    pass
 
 def main():
-    np.random.seed(32)
+    print(__doc__)
+    np.random.seed(processing.SEED)
     X, y = load.iris()
     q_range = (3, 41, 5)
     print(":: Q RANGE: ", q_range)
@@ -90,6 +148,15 @@ def main():
         accs_elm.append([q] + acc_elm)
     accs_elm = np.array(accs_elm)
     print(np.around(accs_elm, decimals=2))
+
+    print(":: Evaluating MLP")
+    print(":: Q | hold-out | 10-fold | leave-one-out")
+    accs_mlp = []
+    for q in range(*q_range):
+        acc_mlp = train_test_mlp(X, y, q=q)
+        accs_mlp.append([q] + acc_mlp)
+    accs_mlp = np.array(accs_mlp)
+    print(np.around(accs_mlp, decimals=2))
 
 
 if __name__ == '__main__':
