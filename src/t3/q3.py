8e6ddf4a9aeba79b716967cfb9ad9f373ed10a19
Manoel Vilela <manoel_vilela@engineer.com>
2019-06-21 15:12:50 -0300

    t3/q3: fix crossover, mutation and make report more readable

--- src/t3/q3.py
+++ src/t3/q3.py
@@ -9,10 +9,12 @@
 #
 
 
-"""G E N E T I C  --  A L G O R I T H M S
-O indíviduo possui 20 bits tal que 10 bits para x e 10 bits para y.
-A função objetiva f deve ser maximizada tal que (x, y) pertence a subfaixa [0,
-20] do conjunto R².
+"""---- G E N E T I C  --  A L G O R I T H M S ----
+O indíviduo possui 20 bits tal que 10 bits para x e 10 bits para y.  A
+função objetiva f deve ser maximizada tal que (x, y) pertence a
+subfaixa [0, 20] do conjunto R².
+
+f(x, y) = |x * sin(y * (pi/4)) + y * sin(x * (pi/4)))|
 """
 
 import math
@@ -20,7 +22,7 @@ import numpy as np
 
 N_BITS = 20
 POPULATION_SIZE = 100
-CONST_SCALE = 20/((2 ** (N_BITS//2)) - 1)
+CONST_SCALE = 20/(2 ** (N_BITS//2) - 1)
 
 def f(x, y):
     return abs(x * math.sin(y * ((math.pi)/4)) + y * math.sin(x * ((math.pi)/4)))
@@ -39,24 +41,69 @@ def bits_to_int(bits):
 
 def decode(bits):
     """Decodifica o indivíduo i em (x, y)"""
-    x = bits_to_int(bits[N_BITS//2:]) * CONST_SCALE
-    y = bits_to_int(bits[:-N_BITS//2]) * CONST_SCALE
+    x = bits_to_int(bits[:N_BITS//2]) * CONST_SCALE
+    y = bits_to_int(bits[N_BITS//2:]) * CONST_SCALE
     return (x, y)
 
 def evaluation(population):
     """Avalia a pontução de cada indíviduo da população"""
-    return [f(*decode(individual)) for individual in population]
+    evaluations = [f(*decode(individual)) for individual in population]
+    population_scored = np.insert(population, N_BITS, evaluations, axis=1)
+    return population_scored
 
 
 def crossover(population):
-    """Cruzamento entre os indíviduos"""
-    # TODO: Implement a crossover method of one-point cut
-    return population
+    """Cruzamento entre os indíviduos: corte de um ponto"""
+
+    np.random.shuffle(population)
+
+    # remove a última coluna com pontuação
+    population = population[:, :-1]
+
+    children = []
+    queue = list(population)
+    while queue:
+        father, mother = queue.pop(), queue.pop()
+        cut_point = np.random.randint(N_BITS)
+
+        fchromo_y = father[cut_point:]
+        mchromo_x = mother[:cut_point]
+        fchromo_x = father[:cut_point]
+        mchromo_y = mother[cut_point:]
+
+        child1 = np.concatenate([fchromo_x, mchromo_y])
+        child2 = np.concatenate([fchromo_y, mchromo_x])
+
+        children.append(child1)
+        children.append(child2)
+
+    return np.array(children)
 
 
-def mutate(population, prob=0.005):
+def random_event(probability):
+    """Realiza um experimento pseudo-aleatório com probabilidade parametrizada.
+
+    Parâmetros
+    ----------
+    probability: deve ser um número real e estar no conjunto (0, 1]
+
+    """
+    if not probability > 0:
+        raise ValueError("probability should be greater than 0.")
+    max_value = 1/probability
+    if np.random.randint(0, max_value) == 0:
+        return True
+    return False
+
+
+
+def mutate(population, probability=0.005):
     """Mutação de indíviduos"""
-    # TODO: try to swap the state of one bit
+    for i in range(len(population)):
+        if random_event(probability):
+          print("MUTATION OCCURS!")
+          j = np.random.randint(0, N_BITS)
+          population[i][j] = 1 - population[i][j]
     return population
 
 
@@ -67,38 +114,61 @@ def selection(population_augmented):
 
 def evolution_step(population):
     # EVOLUTION
-    points = evaluation(population)
-    population_augmented = np.insert(population, N_BITS, points, axis=1)
-    best_individuals = selection(population_augmented)
-    new_population = crossover(best_individuals)
+    population_scored = evaluation(population)
+    best_individuals = selection(population_scored)
+    new_population = mutate(crossover(best_individuals))
     return new_population
 
-def population_report(population):
-    points = evaluation(population)
-    best_individual = population[0]
-    best_point = points[0]
-    for ind, point in zip(population, points):
-        if point > best_point:
-            best_individual = ind
-            best_points = point
-    print("MEAN: ", np.mean(points))
-    print("STD: ", np.std(points))
-    print("MAX: ", np.max(points))
-    print("MIN: ", np.min(points))
-    print("BEST_SOLUTION: ", decode(best_individual))
+def population_report(population, verbose=True):
+    # get best individual
+    population_scored = evaluation(population)
+    evaluations = population_scored[:, N_BITS]
+    population_sorted = population_scored[evaluations.argsort()]
+    best_individual = population_sorted[-1, :-1]
+    best_point = population_sorted[-1]
 
+    # metrics current population
+    mean = np.mean(evaluations)
+    std = np.std(evaluations)
+    max_evaluation = np.max(evaluations)
+    min_evaluation = np.min(evaluations)
+    x_best, y_best = decode(best_individual)
+    if verbose:
+        print("MEAN: ", mean)
+        print("STD: ", std)
+        print("MAX: ", max_evaluation)
+        print("MIN: ", min_evaluation)
+        print("BEST_SOLUTION: ", (x_best, y_best))
+
+    return [x_best, y_best, mean, std, min_evaluation, max_evaluation]
 
 
 def main():
-    print("---- G E N E T I C -- A L G O R I T H M S ---")
+    print(__doc__)
     population = random_population()
-    max_iterations = 10
+    reports = []
+    max_iterations = 100
     i = 0
     while i <= max_iterations:
-        print("---- G E N E R A T I O N = {} ---".format(i))
-        population_report(population)
+        print("---- G E N E R A T I O N [{}] ---".format(i))
+        report = population_report(population, False)
+        reports.append(report)
         population = evolution_step(population)
+
+
         i += 1
+
+    solutions = np.array(reports)
+    _, columns = solutions.shape
+    evaluation_idx = solutions[:, columns - 1].argsort()
+    solutions_sorted = solutions[evaluation_idx]
+    x = solutions_sorted[-1, 0]
+    y = solutions_sorted[-1, 0]
+
+    print("------ B E S T -- S O L U T I O N ------")
+    print("X: {:.2f}".format(x))
+    print("Y: {:.2f}".format(y))
+
 
 
 if __name__ == '__main__':
